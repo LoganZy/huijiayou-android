9769082d63f93d21e6d110eaac03f4fb16244bdb
lugenggeng <lugenggeng@wanglibank.com>
2017-02-23 14:02:31 +0800

    测试

--- app/src/main/java/com/wanglibao/huijiayou/activity/MainActivity.java
+++ app/src/main/java/com/wanglibao/huijiayou/activity/MainActivity.java
@@ -3,14 +3,10 @@ package com.wanglibao.huijiayou.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
 import com.ashokvarma.bottomnavigation.BottomNavigationBar;
 import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.wanglibao.huijiayou.R;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -34,34 +30,6 @@ public class MainActivity extends AppCompatActivity {
                 .addItem(new BottomNavigationItem(R.mipmap.ic_launcher,"账户"))
                 .addItem(new BottomNavigationItem(R.mipmap.ic_launcher,"更多")).initialise();
 
-        List<String> datas = new ArrayList<>();
-        datas.add("1");
-        datas.add("2");
-        datas.add("3");
-        datas.add("4");
 
-        List<String> bendi = new ArrayList<>();
-        bendi.add("2");
-        bendi.add("4");
-
-        List<String> xinde = new ArrayList<>();
-
-        for (String string:datas){
-            for (int i = 0; i < bendi.size(); i++){
-                if (string.equals(bendi.get(i))){
-                    datas.remove(string);
-                    break;
-                }
-                if (i == bendi.size()){
-                    xinde.add(string);
-                }
-            }
-        }
-
-        String value = "";
-        for (String string:xinde){
-            value += string;
-        }
-        Toast.makeText(this,value,Toast.LENGTH_LONG);
     }
 }
