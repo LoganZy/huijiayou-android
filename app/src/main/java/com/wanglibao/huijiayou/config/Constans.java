607ceeda6157f2af8e4d7d250e81a55835957b28
Administrator <1659927986@qq.com>
2017-02-27 17:37:51 +0800

    hexinhai  修改提交

--- app/src/main/java/com/wanglibao/huijiayou/config/Constans.java
+++ app/src/main/java/com/wanglibao/huijiayou/config/Constans.java
@@ -9,16 +9,13 @@ import com.tencent.mm.opensdk.openapi.IWXAPI;
 
 public class Constans {
 
-<<<<<<< HEAD
+
     public final static boolean DEBUG = true;
 
-    public static final String APP_ID = "wx8afbd309ff35e712";
-    public static IWXAPI wx_api;
-=======
-    public static final String WX_APP_ID = "wx8afbd309ff35e712";
+    public static String WX_APP_ID = "wx8afbd309ff35e712";
     public static IWXAPI WXapi;
     public static BaseResp resp;
     public static String AppSecret = "a33465db152afd3bdd86c2fb38b7712b";
->>>>>>> 2cd6c3cc91708379d11ea5908f1b5e53674303c6
+
 
 }
