88e6ef4c0fcc5c7a01e0773cd45fd9b7e687fb4f
Administrator <1659927986@qq.com>
2017-03-02 15:10:43 +0800

    登录界面初步完成

--- app/src/main/java/com/wanglibao/huijiayou/activity/LoginActivity.java
+++ app/src/main/java/com/wanglibao/huijiayou/activity/LoginActivity.java
@@ -1,15 +1,22 @@
 package com.wanglibao.huijiayou.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.Selection;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.tencent.mm.opensdk.constants.ConstantsAPI;
 import com.tencent.mm.opensdk.modelmsg.SendAuth;
 import com.tencent.mm.opensdk.openapi.WXAPIFactory;
@@ -19,12 +26,14 @@ import com.wanglibao.huijiayou.config.Constans;
 import com.wanglibao.huijiayou.jsonrpc.JsonRPCAsyncTask;
 import com.wanglibao.huijiayou.request.RequestInterface;
 import com.wanglibao.huijiayou.utils.LogUtil;
+import com.wanglibao.huijiayou.utils.ToastUtils;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -40,16 +49,21 @@ public class LoginActivity extends BaseActivity {
     TextView tvActivityLoginSendPhoneCode;
     @Bind(R.id.ll_activity_login_invit)
     LinearLayout ll_login_invit;
+    @Bind(R.id.edit_activityLogin_phone)
+    EditText editActivityLoginPhone;
+    @Bind(R.id.btn_activityLogin_login)
+    Button btnActivityLoginLogin;
+    @Bind(R.id.edit_activityLogin_phoneCode)
+    EditText editActivityLoginPhoneCode;
+    @Bind(R.id.edit_activityLogin_invit)
+    EditText editActivityLoginInvit;
     private String weixinCode;
     private String WXBaseUrl = "https://api.weixin.qq.com/";
-    private Handler handler= new Handler()  ;
+    private Handler handler = new Handler();
     private int time = 60;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
     private String telephone;
+    private String SMScode;
+    private String invite;
     // private static String get_access_token = "";
     // 获取第一步的code后，请求以下链接获取access_token
     //public static String GetCodeRequest = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=SECRET&code=CODE&grant_type=authorization_code";
@@ -59,68 +73,14 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
         initTitle();
-        tvTitle.setText("登录");
+        tvTitle.setText("");
         initView();
 
 
-    }
-
-    private void initView() {
-
-        // ImageButton WXLogin = (ImageButton) findViewById(R.id.WXLogin);
-       // TextView tv_SMSVerify = (TextView) findViewById(R.id.tv_activityLogin_sendPhoneCode);
-        EditText et_Telephone = (EditText) findViewById(R.id.edit_activityLogin_phoneCode);
-        telephone = et_Telephone.getText().toString().trim();
-        tvActivityLoginSendPhoneCode.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //显示隐藏的邀请栏
-                ll_login_invit.setVisibility(View.VISIBLE);
-                time = 60;
-                //向服务器请求
-                startTime();
-               // SMSVerify(telephone);
-            }
-        });
-        WXLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                WetLogin();
-            }
-        });
-
-
-    }
-
-    /*
-    * 短信验证的功能
-    *
-    * */
-    private void SMSVerify(String code) {
-
-        String loginUrl = "";
-        JsonRPCAsyncTask jsonRPCAsyncTask = new JsonRPCAsyncTask(LoginActivity.this, loginUrl, "messageAuth", null, "jsonObject", 1, code);
-        jsonRPCAsyncTask.execute();
-    }
-
-    private void WetLogin() {
-        Constans.WXapi = WXAPIFactory.createWXAPI(this, Constans.WX_APP_ID, true);
-        if (!Constans.WXapi.isWXAppInstalled()) {
-            Toast.makeText(this, "请先安装微信应用", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if (!Constans.WXapi.isWXAppSupportAPI()) {
-            Toast.makeText(this, "请先更新微信应用", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        Constans.WXapi.registerApp(Constans.WX_APP_ID);
-        SendAuth.Req req = new SendAuth.Req();
-        req.scope = "snsapi_userinfo";
-        req.state = "wechat_sdk_demo";
-        Constans.WXapi.sendReq(req);
     }
 
     @Override
@@ -133,6 +93,156 @@ public class LoginActivity extends BaseActivity {
 
         }
     }
+
+    private void initView() {
+
+
+        WXLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //WetLogin();
+                startActivity(new Intent(LoginActivity.this,WXBindActivity.class));
+            }
+        });
+
+
+
+        setEditTextInhibitInputSpace(editActivityLoginPhone);
+        // ImageButton WXLogin = (ImageButton) findViewById(R.id.WXLogin);
+        // TextView tv_SMSVerify = (TextView) findViewById(R.id.tv_activityLogin_sendPhoneCode);
+        // EditText et_Telephone = (EditText) findViewById(R.id.edit_activityLogin_phoneCode);
+//        edittext 关于电话号码的逻辑
+        editActivityLoginPhone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s == null || s.length() == 0)
+                    return;
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < s.length(); i++) {
+                    if (i != 3 && i != 8 && s.charAt(i) == ' ') {
+                        continue;
+                    } else {
+                        sb.append(s.charAt(i));
+                        if ((sb.length() == 4 || sb.length() == 9)
+                                && sb.charAt(sb.length() - 1) != ' ') {
+                            sb.insert(sb.length() - 1, ' ');
+                        }
+                    }
+                }
+                if (!sb.toString().equals(s.toString())) {
+                    int index = start + 1;
+                    if (sb.charAt(start) == ' ') {
+                        if (before == 0) {
+                            index++;
+                        } else {
+                            index--;
+                        }
+                    } else {
+                        if (before == 1) {
+                            index--;
+                        }
+                    }
+                    editActivityLoginPhone.setText(sb.toString());
+                    editActivityLoginPhone.setSelection(index);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int len = editActivityLoginPhone.getText().length();
+                if (len > 13) {
+                    int selEndIndex = Selection.getSelectionEnd(editActivityLoginPhone.getText());
+                    String str = editActivityLoginPhone.getText().toString();
+                    //截取新字符串
+                    String newStr = str.substring(0, 13);
+                    editActivityLoginPhone.setText(newStr);
+
+                }
+
+            }
+        });
+
+        tvActivityLoginSendPhoneCode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                telephone = editActivityLoginPhone.getText().toString().trim();
+                SMScode = editActivityLoginPhoneCode.getText().toString().trim();
+                if(TextUtils.isEmpty(telephone)||telephone==null){
+                    ToastUtils.createLongToast(LoginActivity.this, "请输入手机号！");
+                }else if (!telephone.startsWith("1") || telephone.length() != 13) {
+                    ToastUtils.createLongToast(LoginActivity.this, "手机号码格式不正确，请重新输入！");
+                }else if(TextUtils.isEmpty(SMScode)) {
+                    ToastUtils.createLongToast(LoginActivity.this, "请输入短信接收到的验证码");
+                    ll_login_invit.setVisibility(View.VISIBLE);
+                    time = 60;
+                    //向服务器请求
+                    startTime();
+                }
+
+            }
+
+            /*
+            * 短信验证的功能
+            *
+            * */
+            private void SMSVerify(String code) {
+
+                String loginUrl = "";
+                JsonRPCAsyncTask jsonRPCAsyncTask = new JsonRPCAsyncTask(LoginActivity.this, loginUrl, "messageAuth", null, "jsonObject", 1, code);
+                jsonRPCAsyncTask.execute();
+            }
+
+
+
+            /*
+            *
+            * 发送定时消息的方法
+            *
+            * */
+            public void startTime() {
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        time -= 1;
+                        if (time <= 0) {
+                            handler.removeCallbacksAndMessages(null);
+                            tvActivityLoginSendPhoneCode.setEnabled(true);
+                            tvActivityLoginSendPhoneCode.setText("重新获取");
+                        } else {
+                            tvActivityLoginSendPhoneCode.setEnabled(false);
+                            tvActivityLoginSendPhoneCode.setText(time + "s");
+                            handler.postDelayed(this, 1000);
+                        }
+                    }
+                }, 1000);
+
+            }
+        });
+    }
+
+    private void WetLogin() {
+        Constans.WXapi = WXAPIFactory.createWXAPI(LoginActivity.this, Constans.WX_APP_ID, true);
+        if (!Constans.WXapi.isWXAppInstalled()) {
+            Toast.makeText(LoginActivity.this, "请先安装微信应用", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (!Constans.WXapi.isWXAppSupportAPI()) {
+            Toast.makeText(LoginActivity.this, "请先更新微信应用", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Constans.WXapi.registerApp(Constans.WX_APP_ID);
+        SendAuth.Req req = new SendAuth.Req();
+        req.scope = "snsapi_userinfo";
+        req.state = "wechat_sdk_demo";
+        Constans.WXapi.sendReq(req);
+    }
+
 
     /*
     * 获取微信的token等信息
@@ -166,27 +276,43 @@ public class LoginActivity extends BaseActivity {
             }
         });
     }
-    /*
-    *
-    * 发送定时消息的方法
-    *
-    * */
-    public void startTime() {
-        handler.postDelayed(new Runnable() {
+
+    /**
+     * 禁止EditText输入空格
+     *
+     * @param editText
+     */
+    public  void setEditTextInhibitInputSpace(EditText editText) {
+        InputFilter filter = new InputFilter() {
             @Override
-            public void run() {
-                time -= 1;
-                if (time <= 0) {
-                    handler.removeCallbacksAndMessages(null);
-                    tvActivityLoginSendPhoneCode.setEnabled(true);
-                    tvActivityLoginSendPhoneCode.setText("重新获取");
+            public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+                if (source.equals(" ")) {
+                    return "";
                 } else {
-                    tvActivityLoginSendPhoneCode.setEnabled(false);
-                    tvActivityLoginSendPhoneCode.setText(time + "s");
-                    handler.postDelayed(this, 1000);
+                    return null;
                 }
             }
-        }, 1000);
+        };
+        editText.setFilters(new InputFilter[]{filter});
+    }
+    /*
+    * 点击登录
+    * */
+    @OnClick(R.id.btn_activityLogin_login)
+    public void onClick() {
+        telephone = editActivityLoginPhone.getText().toString().trim();
+        SMScode = editActivityLoginPhoneCode.getText().toString().trim();
+        invite  =editActivityLoginInvit.getText().toString().trim();
+        if(TextUtils.isEmpty(telephone)||telephone==null){
+                ToastUtils.createLongToast(LoginActivity.this, "请输入手机号！");
+        }else if (!telephone.startsWith("1") || telephone.length() != 13) {
+                ToastUtils.createLongToast(LoginActivity.this, "手机号码格式不正确，请重新输入！");
+        }else if(TextUtils.isEmpty(SMScode)){
+            ToastUtils.createLongToast(LoginActivity.this, "请输入短信接收到的验证码");
+        }else{
+            //请求网络
+            ToastUtils.createLongToast(LoginActivity.this, "手机正确，谢谢输入！");
+        }
 
     }
 }
